2e865ad77bb4fad3aac1bebd3d1aa39087a06699
Julian Calle <julycf83@gmail.com>
2018-05-20 12:27:37 +0200

    limpieza ficheros

--- js/load.js
+++ js/load.js
@@ -4,40 +4,6 @@ $(document).ready(function() {
         interval: 3500
     });
 
-	
-	
-	
-    /*var scroll_start = 0;
-	var ee = $('ejemplo');
-	var div_alto = $("#carousel-example-generic").height();
-	$(".icon-bar").css('background-color', '#ffffff');
-
-	
-       $(document).scroll(function() { 
-          scroll_start = $(this).scrollTop();
-          if(scroll_start > (div_alto - 85)) {
-              $(".navbar-default").css({'background-color':'#ffffff', 'transition' : '1s', 'opacity' : '0.8'});
-              $("#ejemplo").css('position' , 'fixed');
-              $("#logo1").hide();
-              $("#logo2").show();
-              $("#menuHover > li > a").css('color', '#404a3d');
-              $(".icon-bar").css('background-color', '#888');
-
-           } else {
-              $('.navbar-default').css({'background-color': 'transparent' , 'transition' : 'none'});
-              $("#ejemplo").css({'position' : 'absolute', 'z-index' : '999'});
-              $("#logo2").hide();
-              $("#logo1").show();
-               $("#menuHover > li > a").css('color', '#ffffff');
-               $(".icon-bar").css('background-color', '#ffffff');
-           }
-
-       });*/
-    
-    
-    
-
-
 	var inputNombre = $("#inputnombre");
 	var labelNombre = $("#labelnombre");
 	var inputEmail = $("#inputEmail");
@@ -64,45 +30,37 @@ $(document).ready(function() {
 	} else {
 		labelMensaje.css({ 'top' : '-30px', 'opacity': '0.3'});
 	}
-	
-	
-	inputNombre.focus(function() {
-	  		labelNombre.css({ 'top' : '-30px', 'opacity': '0.3'});
 
+	inputNombre.focus(function() {
+		labelNombre.css({ 'top' : '-30px', 'opacity': '0.3'});
 	});
 	
 	inputNombre.focusout(function() {
 		if(inputNombre.val() == "") {
 	  		labelNombre.css({'top' : '0', 'opacity': '1'});
 		}
-
 	});
 	
 	inputEmail.focus(function() {
 	  		labelEmail.css({ 'top' : '-30px', 'opacity': '0.3'});
-
 	});
 	
 	inputEmail.focusout(function() {
 		if(inputEmail.val() == "") {
 	  		labelEmail.css({'top' : '0', 'opacity': '1'});
 		}
-
 	});
 	
 	inputMensaje.focus(function() {
 	  		labelMensaje.css({ 'top' : '-30px', 'opacity': '0.3'});
-
 	});
 	
 	inputMensaje.focusout(function() {
 		if(inputMensaje.val() == "") {
 	  		labelMensaje.css({'top' : '0', 'opacity': '1'});
 		}
-
 	});
-	
-	
+
 	
 	//Efectos redes sociales
 	
@@ -139,41 +97,8 @@ $(document).ready(function() {
         linkedin2.hide();
 		linkedin1.show();
     });
-	
-	
-/*
-	var anchoPantalla = screen.width;
-	
-	
-	var tituloImgInicio = $('.tituloImgInicio').width();
-	var subtituloImgInicio = $('.subtituloImgInicio').width();
-	var tituloImgInicio2 = $('.tituloImgInicio2').width();
-	var subtituloImgInicio2 = $('.subtituloImgInicio2').width();
-	
-	$('.tituloImgInicio').animate({
-          'margin-left':  (anchoPantalla - tituloImgInicio - 16)/2
-	},1500);
-	
-	$('.subtituloImgInicio').animate({
-          'margin-right':  (anchoPantalla - subtituloImgInicio - 16 )/2 
-	},1500);
-	
-	$('.tituloImgInicio2').animate({
-          'margin-left':  (anchoPantalla - tituloImgInicio2 - 16)/2
-	},1500);
-	
-	$('.subtituloImgInicio2').animate({
-          'margin-right':  (anchoPantalla - subtituloImgInicio2 - 16 )/2 
-	},1500);
-    
-    
-    
-    */
 
-    
-    
     var scroll_start = 0;
-	
     var wrap = $("#navmain");
     var ww = $('#ww');
     
@@ -182,25 +107,16 @@ $(document).ready(function() {
       scroll_start = $(this).scrollTop();
         
         if(scroll_start) {
-          
            wrap.addClass("menuScrollGrande");
             ww.addClass("ww");
-         
        } else {
-           
            wrap.removeClass("menuScrollGrande");
            ww.removeClass("ww");
        }
-	   
    });
     
     $('.btnMenu').click(function() {
-
         $("#hamburguer").attr("checked",false);
-
-    
     });
-
-	
 });
 
